9fe5444743e32396ce794704ca9489910cd6457a
Brian Broll <brian.broll@vanderbilt.edu>
2017-06-24 14:53:10 -0500

    Added support for two users occupying a single role. Fixes #1006 (#1219)
    * WIP #1006 Added some support for two sockets at a single role
    
    * WIP #1006 Updated for multiple occupants at a role
    
    * WIP #1006 updated for multiple sockets at a role
    
    * WIP #1006 comments for where to update the roles
    
    * WIP #1006 updated forking room, role deletion and eviction
    
    * WIP misc vantage updates
    
    * WIP #1006 updated room.move and room.add/remove
    
    * WIP #1006 misc updates and code cleanup
    
    * WIP #1006 room.close() should return promise
    
    * WIP #1006 updated socket retrieval
    
    * WIP #1006 rename role update and checkRoom
    
    * WIP #1006 moveToRole, hasRole and misc updates
    
    * WIP #1006 updated tests
    
    * WIP #1006 added more tests for room and sockets
    
    * WIP #1006 updating the room tab
    
    * WIP #1006 Added tests for routing msgs in a room
    
    * WIP #1006 Assign the room if socket added to role
    
    * WIP #1006 updated collaboration to use the room info
    
    * WIP #1006 Added test for joining roles
    
    * WIP #1006 Improved efficiency of moving around roles
    
    * WIP #1006 updated vantage for multi socket support on roles
    
    * WIP #1006 server relays user actions back to shared sockets
    
    * WIP #1006 open least occupied role when joining active
    
    * WIP #1006 removed old dependency
    
    * WIP #1006 Fixed first load error
    
    * WIP #1006 updated snap
    
    * WIP #1006 fixed linting issues and eviction
    
    * WIP #1006 missing semi
    
    * WIP #1006 updated tests (removed namespace)

--- test/assets/utils.js
+++ test/assets/utils.js
@@ -1,5 +1,93 @@
-var _ = require('lodash'),
-    assert = require('assert');
+/*global Client*/
+const  _ = require('lodash');
+const assert = require('assert');
+
+// load the *exact* XML_Serializer from Snap!... pretty hacky...
+const path = require('path');
+const fs = require('fs');
+const PROJECT_ROOT = path.join(__dirname, '..', '..');
+const ActiveRoom = require(PROJECT_ROOT + '/src/server/rooms/active-room');
+const NetsBloxSocket = require(PROJECT_ROOT + '/src/server/rooms/netsblox-socket');
+const Socket = require('./mock-websocket');
+const Logger = require(PROJECT_ROOT + '/src/server/logger');
+
+(function() {
+    var clientDir = path.join(PROJECT_ROOT, 'src', 'client', 'Snap--Build-Your-Own-Blocks'),
+        srcFiles = ['morphic.js', 'xml.js', 'store.js', 'actions.js'],
+        src;
+
+    src = srcFiles
+        .map(file => path.join(clientDir, file))
+        .map(file => {
+            var code = fs.readFileSync(file, 'utf8');
+            if (file.includes('morphic.js')) {
+                code = code
+                    .split('// Morph')[0]
+                    .split('// Global Functions')[1];
+            }
+        
+            if (file.includes('store.js')) {  // remove the SnapSerializer stuff
+                code = code.split('StageMorph.prototype.toXML')[0];
+            }
+            return code;
+        })
+        .join('\n');
+
+
+    // expose the XML_Serializer
+    src = [
+        'modules = {};',
+        'window = {location:{}};',
+        'var SnapActions;',
+        src,
+        'global.Client = global.Client || {};',
+        'global.Client.XML_Serializer = XML_Serializer;',
+        'global.Client.SnapActions = SnapActions;'
+    ].join('\n');
+    eval(src);
+})(this);
+
+// Test loading of xml
+const idBlocks = block => {
+    block.attributes.collabId = 'testId';
+    block.children.forEach(child => idBlocks(child));
+    return block;
+};
+
+const parser = new Client.XML_Serializer();
+const canLoadXml = string => {
+    var xml;
+
+    // Add a collabId and reserialize
+    var res = Client.SnapActions.uniqueIdForImport(string);
+    xml = res.toString();
+    assert(parser.parse(xml));
+};
+
+// Create configured room helpers
+let logger = new Logger('netsblox:test');
+const createSocket = function(username) {
+    const socket = new NetsBloxSocket(logger, new Socket());
+    socket.uuid = `_netsblox${Date.now()}`;
+    socket.username = username || socket.uuid;
+    return socket;
+};
+
+const createRoom = function(config) {
+    const room = new ActiveRoom(logger, config.name, config.owner);
+
+    Object.keys(config.roles).forEach(name => {
+        config.roles[name] = config.roles[name] || [];
+        room.silentCreateRole(name);
+        config.roles[name].forEach(username => {
+            const socket = createSocket(username);
+
+            room.silentAdd(socket, name);
+        });
+    });
+
+    return room;
+};
 
 module.exports = {
     verifyRPCInterfaces: function(rpc, interfaces) {
@@ -14,5 +102,10 @@ module.exports = {
                 });
             });
         });
-    }
+    },
+    XML_Serializer: Client.XML_Serializer,
+    canLoadXml: canLoadXml,
+
+    createRoom: createRoom,
+    createSocket: createSocket
 };
